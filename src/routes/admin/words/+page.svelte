303593150b19fe915e35e8d23d9db60a3bdbc23f
Musicode1 <MusiCode3@gmail.com>
2025-12-06 02:52:04 +0200

    ניקיון קוד: הסרת רכיבי אדמין ישנים שאינם בשימוש

--- src/routes/admin/words/+page.svelte
+++
@@ -1,9 +0,0 @@
-<script lang="ts">
-	import WordForm from '$lib/components/admin/WordForm.svelte';
-	import WordList from '$lib/components/admin/WordList.svelte';
-</script>
-
-<div class="space-y-8">
-	<WordForm />
-	<WordList />
-</div>
